d1e4e209f5eaad1766c6368b69ccd9fef6fec26d
VAIBHAVI R. BHURANE <117935143+vaibhavibhurane@users.noreply.github.com>
2024-03-16 19:06:49 +0530

    Update settings.py

--- BharatYatra/BharatYatra/settings.py
+++ BharatYatra/BharatYatra/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q^_$%j@7(w(ms3n&=-$r5)(el)ht23y2d4%5e$s-*8svjc9ljg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
